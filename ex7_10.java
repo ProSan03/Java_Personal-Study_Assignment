13847c2e723c3c6eb24911dbfa47db13e3b29532
Yeongsan <65652613+ProSan03@users.noreply.github.com>
2024-06-08 14:58:13 +0900

    Update ex7_10.java
    교재 7장 10번 실습 문제(완성)

--- ex7_10.java
+++ ex7_10.java
@@ -4,19 +4,19 @@ abstract class Shape {
 	public abstract void draw(); // 추상 메소드
 }
 
-class Line extends Shape {
+class Line extends Shape { // Line 구현
 	public void draw() {
 		System.out.println("Line");
 	}
 }
 
-class Rect extends Shape {
+class Rect extends Shape { // Rect 구현
 	public void draw() {
 		System.out.println("Rect");
 	}
 }
 
-class Circle extends Shape {
+class Circle extends Shape { // Circle 구현
 	public void draw() {
 		System.out.println("Circle");
 	}
@@ -24,29 +24,29 @@ class Circle extends Shape {
 
 public class ex7_10 {
 	public static void main(String[] args) {
-		// 문자열 값만 다루는 제네릭 벡터 생성
-		Vector<Shape> v = new Vector<Shape>(3);
+		// 객체 Shape 값만 다루는 제네릭 벡터 생성
+		Vector<Shape> v = new Vector<Shape>(3); // 벡터 크기는 3으로 초기화
 		System.out.println("그래픽 에디터 beauty를 실행합니다. ");
 		boolean T = true;
 		Scanner sc = new Scanner(System.in);
 
 		while (T) {
-			System.out.println("삽입(1), 삭제(2), 모두 보기(3), 종료(4)>> ");
-			
+			System.out.print("삽입(1), 삭제(2), 모두 보기(3), 종료(4)>> ");
+
 			int userInput = sc.nextInt();
 			switch (userInput) {
 			case 1: // 삽입부 구현
-				System.out.println("Line(1), Rect(2), Circle(3)>> ");
+				System.out.print("Line(1), Rect(2), Circle(3)>> ");
 				int selectShape = sc.nextInt();
 				switch (selectShape) {
 				case 1:
-					v.add(new Line());
+					v.add(new Line()); // 1을 누를시 Line 생성
 					break;
 				case 2:
-					v.add(new Rect());
+					v.add(new Rect()); // 2를 누를시 Rect 생성
 					break;
 				case 3:
-					v.add(new Circle());
+					v.add(new Circle()); // 3을 누를시 circle 생성
 					break;
 				default:
 					System.out.println("잘못된 입력입니다.");
@@ -54,22 +54,19 @@ public class ex7_10 {
 				break;
 
 			case 2: // 삭제부 구현
-				System.out.println("삭제할 도형의 위치>> ");
+				System.out.print("삭제할 도형의 위치>> ");
 				int deletePoint = sc.nextInt();
-				// 없는 위치에 접근할 경우 예외가 발생하므로, 예외 처리를 따로 해줘야함
-				try {
-				v.remove(deletePoint-1); // -1 해줘야 원하는 위치에 접근가능 (배열은 0부터 시작)
-				}
-				
-				catch(ArrayIndexOutOfBoundsException e) { // 도형이 없는 위치에 접근할 경우 출력
+				if (deletePoint > 0 && deletePoint <= v.size()) { // 삭제하는 위치가 0보다 커야하고, 벡터의 총 크기 이하여야 "ArrayIndexOutOfBounds" 예외 피할 수 있음
+					v.remove(deletePoint - 1); // -1 해줘야 원하는 위치에 접근가능 (배열은 0부터 시작)
+				} else {
 					System.out.println("삭제할 위치에 도형이 없습니다.");
 				}
 				break;
 
 			case 3: // 모두 보기
 				for (int i = 0; i < v.size(); i++) {
-					Shape p = v.get(i); // 벡터에서 i번째 String 값 얻어내기
-					System.out.println(p); // 해당하는 i번째 String 값 출력
+					Shape p = v.get(i); // 벡터에서 i번째 Shape 값 얻어내기
+					p.draw(); // 해당 도형의 draw 메소드 호출
 				}
 				break;
 
@@ -77,7 +74,12 @@ public class ex7_10 {
 				System.out.println("beauty를 종료합니다.");
 				T = false;
 				break;
+
+			default:
+				System.out.println("잘못된 입력입니다.");
 			}
 		}
+
+		sc.close();
 	}
 }
